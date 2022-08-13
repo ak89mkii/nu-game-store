3b337853e49011b196875d805930dea5160a9091
Alex Kasem <spock-117@Alexs-MacBook-Pro.local>
2022-08-13 17:06:55 -0400

    Changed brand imagge CSS.

--- src/components/BGInfo/BGInfo.jsx
+++ src/components/BGInfo/BGInfo.jsx
@@ -14,7 +14,7 @@ const BGInfo = (props) => {
             <br></br>
             <Container>
             <h1>Tabletop Game Top Brands</h1>
-            <img className='infoImage' src={Sega}></img>
+            <img className='infoImage' src='https://cf.geekdo-images.com/Waerw1N-m4Gc7zZaG8p6rw__itemrep/img/KkoGnWyzcgjA2q0GPahnXXmkFSI=/fit-in/246x300/filters:strip_icc()/pic4154095.png'></img>
             <img className='infoImage' src='https://pngimg.com/uploads/sony_playstation/sony_playstation_PNG17532.png'></img>
             <img className='infoImage' src='https://progressbar.com.au/wp-content/uploads/2017/06/Nintendo-Banner.png'></img>
             <img className='infoImage' src='https://download.logo.wine/logo/Xbox/Xbox-Logo.wine.png'></img>
