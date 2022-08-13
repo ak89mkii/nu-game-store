df59cf291c26cd2ad90bd10d1f46b98cf573aab1
Alex Kasem <spock-117@Alexs-MacBook-Pro.local>
2022-08-12 20:38:54 -0400

    Added 'VGInfo' component.

--- src/components/VGInfo/VGInfo.jsx
+++ src/components/VGInfo/VGInfo.jsx
@@ -16,8 +16,14 @@ const VGInfo = (props) => {
             <h1>Video Game Top Brands</h1>
             <img className='infoImage' src={Sega}></img>
             <img className='infoImage' src='https://pngimg.com/uploads/sony_playstation/sony_playstation_PNG17532.png'></img>
-            <img className='infoImage' src='https://m.media-amazon.com/images/I/51wZ3cmIyOL.jpg'></img>
-            <img className='infoImage' src='https://m.media-amazon.com/images/I/51wZ3cmIyOL.jpg'></img>
+            <img className='infoImage' src='https://progressbar.com.au/wp-content/uploads/2017/06/Nintendo-Banner.png'></img>
+            <img className='infoImage' src='https://download.logo.wine/logo/Xbox/Xbox-Logo.wine.png'></img>
+            <img className='infoImage' src={Sega}></img>
+            <img className='infoImage' src='https://pngimg.com/uploads/sony_playstation/sony_playstation_PNG17532.png'></img>
+            <img className='infoImage' src='https://progressbar.com.au/wp-content/uploads/2017/06/Nintendo-Banner.png'></img>
+            <img className='infoImage' src='https://download.logo.wine/logo/Xbox/Xbox-Logo.wine.png'></img>
+            <img className='infoImage' src='https://progressbar.com.au/wp-content/uploads/2017/06/Nintendo-Banner.png'></img>
+            <img className='infoImage' src='https://download.logo.wine/logo/Xbox/Xbox-Logo.wine.png'></img>
             </Container>
             <hr class="bg-warning border-2 border-top border-warning" />
         </div>
