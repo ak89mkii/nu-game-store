f6c5e6814dfa554d754f6176f4365a12c5848f1a
Alex Kasem <spock-117@Alexs-MacBook-Pro.local>
2022-08-13 21:30:12 -0400

    Changed brand imagge CSS.

--- src/components/BGInfo/BGInfo.jsx
+++ src/components/BGInfo/BGInfo.jsx
@@ -15,15 +15,12 @@ const BGInfo = (props) => {
             <Container>
             <h1>Tabletop Game Top Brands</h1>
             <img className='infoImage' src='https://cf.geekdo-images.com/Waerw1N-m4Gc7zZaG8p6rw__itemrep/img/KkoGnWyzcgjA2q0GPahnXXmkFSI=/fit-in/246x300/filters:strip_icc()/pic4154095.png'></img>
-            <img className='infoImage' src='https://pngimg.com/uploads/sony_playstation/sony_playstation_PNG17532.png'></img>
-            <img className='infoImage' src='https://progressbar.com.au/wp-content/uploads/2017/06/Nintendo-Banner.png'></img>
-            <img className='infoImage' src='https://download.logo.wine/logo/Xbox/Xbox-Logo.wine.png'></img>
-            <img className='infoImage' src={Sega}></img>
-            <img className='infoImage' src='https://pngimg.com/uploads/sony_playstation/sony_playstation_PNG17532.png'></img>
-            <img className='infoImage' src='https://progressbar.com.au/wp-content/uploads/2017/06/Nintendo-Banner.png'></img>
-            <img className='infoImage' src='https://download.logo.wine/logo/Xbox/Xbox-Logo.wine.png'></img>
-            <img className='infoImage' src='https://progressbar.com.au/wp-content/uploads/2017/06/Nintendo-Banner.png'></img>
-            <img className='infoImage' src='https://download.logo.wine/logo/Xbox/Xbox-Logo.wine.png'></img>
+            <img className='infoImage' src='http://assets.stickpng.com/thumbs/6214feecba3c68672b23d6ce.png'></img>
+            <img className='infoImage' src='https://boardgametoday.com/wp-content/uploads/2019/01/fantasy-flight-games-logo.jpg'></img>
+            <img className='infoImage' src='https://cdn.shopify.com/s/files/1/0255/2168/4558/files/UltraPRO_Black_68d1e578-c59f-486f-a491-025627dc9078.jpg?v=1623191442'></img>
+            <img className='infoImage' src='https://assets.pokemon.com/assets//cms2/img/trading-card-game/_tiles/tcg_product_gallery_169_en.jpg'></img>
+            <img className='infoImage' src='https://www.yugioh-card.com/en/wp-content/uploads/2020/09/TCG_logo_500x500.png'></img>
+            <img className='infoImage' src='https://logos-world.net/wp-content/uploads/2021/12/DnD-Symbol.png'></img>
             </Container>
             <hr class="bg-warning border-2 border-top border-warning" />
         </div>
--- src/components/VGInfo/VGInfo.jsx
+++ src/components/VGInfo/VGInfo.jsx
@@ -18,12 +18,9 @@ const VGInfo = (props) => {
             <img className='infoImage' src='https://pngimg.com/uploads/sony_playstation/sony_playstation_PNG17532.png'></img>
             <img className='infoImage' src='https://progressbar.com.au/wp-content/uploads/2017/06/Nintendo-Banner.png'></img>
             <img className='infoImage' src='https://download.logo.wine/logo/Xbox/Xbox-Logo.wine.png'></img>
-            <img className='infoImage' src={Sega}></img>
-            <img className='infoImage' src='https://pngimg.com/uploads/sony_playstation/sony_playstation_PNG17532.png'></img>
-            <img className='infoImage' src='https://progressbar.com.au/wp-content/uploads/2017/06/Nintendo-Banner.png'></img>
-            <img className='infoImage' src='https://download.logo.wine/logo/Xbox/Xbox-Logo.wine.png'></img>
-            <img className='infoImage' src='https://progressbar.com.au/wp-content/uploads/2017/06/Nintendo-Banner.png'></img>
-            <img className='infoImage' src='https://download.logo.wine/logo/Xbox/Xbox-Logo.wine.png'></img>
+            <img className='infoImage' src='https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,f_auto,q_auto:eco,dpr_1/v1501533252/wa2tgvekdjm7aa8qrhv8.png'></img>
+            <img className='infoImage' src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Activision-blizzard_logo_Black.svg/1200px-Activision-blizzard_logo_Black.svg.png'></img>
+            <img className='infoImage' src='https://i.ytimg.com/vi/F7GTpD_kkWQ/hqdefault.jpg'></img>
             </Container>
             <hr class="bg-warning border-2 border-top border-warning" />
         </div>
