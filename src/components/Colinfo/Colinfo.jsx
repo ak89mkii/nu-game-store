7313c98dbbe7967df5265fe41f6799a0cac5f007
Alex Kasem <spock-117@Alexs-MacBook-Pro.local>
2022-08-15 15:56:00 -0400

    Added 'ColInfo' componennt and CSS.

--- src/components/Colinfo/Colinfo.jsx
+++ src/components/Colinfo/Colinfo.jsx
@@ -14,8 +14,8 @@ const ColInfo = (props) => {
             <br></br>
             <Container>
             <h1 className='infoTitleCol'>Collectibles Top Brands</h1>
-            <img className='infoImage' src={Sega}></img>
-            <img className='infoImage' src='https://pngimg.com/uploads/sony_playstation/sony_playstation_PNG17532.png'></img>
+            <img className='infoImage' src='https://i5.walmartimages.com/dfw/4ff9c6c9-79db/k2-_494546fa-37fb-4b4e-9e82-26b7ce0c0658.v1.jpg'></img>
+            <img className='infoImage' src='https://upload.wikimedia.org/wikipedia/commons/1/1c/Gunpla_Logo.svg'></img>
             <img className='infoImage' src='https://progressbar.com.au/wp-content/uploads/2017/06/Nintendo-Banner.png'></img>
             <img className='infoImage' src='https://download.logo.wine/logo/Xbox/Xbox-Logo.wine.png'></img>
             <img className='infoImage' src={Sega}></img>
