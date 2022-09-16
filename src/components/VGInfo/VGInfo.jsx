89092ee0e8f1749101ec31abee13073448e3fb9e
Alex Kasem <spock-117@Alexs-MacBook-Pro.local>
2022-09-16 16:46:22 -0400

    Fixed 'show more' button function.

--- src/components/VGInfo/VGInfo.jsx
+++ src/components/VGInfo/VGInfo.jsx
@@ -11,9 +11,9 @@ const VGInfo = (props) => {
             <br></br>
             <Container>
             <h1 className='infoTitleVG'>Video Game Top Brands</h1>
-            <img className='infoImage' src={Sega}></img>
+            <img className='infoImage' src={Sega} alt='Sega'></img>
             <img className='infoImage' src='https://pngimg.com/uploads/sony_playstation/sony_playstation_PNG17532.png'></img>
-            <img className='infoImage' src='https://progressbar.com.au/wp-content/uploads/2017/06/Nintendo-Banner.png'></img>
+            <img className='infoImage' src='https://seeklogo.com/images/N/nintendo-switch-logo-FC17BE5E92-seeklogo.com.png'></img>
             <img className='infoImage' src='https://download.logo.wine/logo/Xbox/Xbox-Logo.wine.png'></img>
             <img className='infoImage' src='https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,f_auto,q_auto:eco,dpr_1/v1501533252/wa2tgvekdjm7aa8qrhv8.png'></img>
             <img className='infoImage' src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Activision-blizzard_logo_Black.svg/1200px-Activision-blizzard_logo_Black.svg.png'></img>
